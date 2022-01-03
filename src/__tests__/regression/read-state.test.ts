a33210bb67b6a7025da0283e6bdb4b3d6b1bab3e
ppedziwiatr <peter@redstone.finance>
2022-01-03 09:59:56 +0100

    test: stable stringify in tests, new regression testcase

--- src/__tests__/regression/read-state.test.ts
+++ src/__tests__/regression/read-state.test.ts
@@ -5,6 +5,8 @@ import { interactRead, readContract } from 'smartweave';
 import Arweave from 'arweave';
 import { LoggerFactory, SmartWeaveNodeFactory } from '@smartweave';
 
+const stringify = require('safe-stable-stringify')
+
 function* chunks(arr, n) {
   for (let i = 0; i < arr.length; i += n) {
     // note: wrapping with an array to make it compatible with describe.each
@@ -34,10 +36,10 @@ describe.each(chunked)('.suite %#', (contracts: string[]) => {
     async (contractTxId: string) => {
       console.log('readContract', contractTxId);
       const result = await readContract(arweave, contractTxId);
-      const resultString = JSON.stringify(result).trim();
+      const resultString = stringify(result).trim();
       console.log('readState', contractTxId);
       const result2 = await SmartWeaveNodeFactory.memCached(arweave, 1).contract(contractTxId).readState();
-      const result2String = JSON.stringify(result2.state).trim();
+      const result2String = stringify(result2.state).trim();
       expect(result2String).toEqual(resultString);
     },
     600000
@@ -49,13 +51,13 @@ describe('readState', () => {
     const contractTxId = 'CbGCxBJn6jLeezqDl1w3o8oCSeRCb-MmtZNKPodla-0';
     const blockHeight = 707892;
     const result = await readContract(arweave, contractTxId, blockHeight);
-    const resultString = JSON.stringify(result).trim();
+    const resultString = stringify(result).trim();
 
     const result2 = await SmartWeaveNodeFactory.memCached(arweave, 1)
       .contract(contractTxId)
       .setEvaluationOptions({ updateCacheForEachInteraction: false })
       .readState(blockHeight);
-    const result2String = JSON.stringify(result2.state).trim();
+    const result2String = stringify(result2.state).trim();
 
     expect(result2String).toEqual(resultString);
   });
--- tools/contract.ts
+++ tools/contract.ts
@@ -1,15 +1,24 @@
 /* eslint-disable */
 import Arweave from 'arweave';
-import {LoggerFactory, RedstoneGatewayInteractionsLoader, SmartWeaveWebFactory} from '../src';
+import {
+  LoggerFactory, MemCache, RedstoneGatewayContractDefinitionLoader,
+  RedstoneGatewayInteractionsLoader,
+  SmartWeaveWebFactory
+} from '../src';
+
 import {TsLogFactory} from '../src/logging/node/TsLogFactory';
 import fs from 'fs';
 import path from 'path';
 import {FromFileInteractionsLoader} from './FromFileInteractionsLoader';
 import {SmartWeaveNodeFactory} from '../src/core/node/SmartWeaveNodeFactory';
+import {readContract} from "smartweave";
+
+const stringify = require('safe-stable-stringify')
+
 
 const logger = LoggerFactory.INST.create('Contract');
 
-// LoggerFactory.use(new TsLogFactory());
+//LoggerFactory.use(new TsLogFactory());
 LoggerFactory.INST.logLevel("error");
 LoggerFactory.INST.logLevel("debug", "ArweaveGatewayInteractionsLoader");
 LoggerFactory.INST.logLevel("debug", "HandlerBasedContract");
@@ -27,14 +36,19 @@ async function main() {
     logging: false // Enable network request logging
   });
 
-  const contractTxId = 'Daj-MNSnH55TDfxqC7v4eq0lKzVIwh98srUaWqyuZtY';
-  //const contractTxId = 't9T7DIOGxx4VWXoCEeYYarFYeERTpWIC1V3y-BPZgKE';
+  //const contractTxId = 'Daj-MNSnH55TDfxqC7v4eq0lKzVIwh98srUaWqyuZtY';
+  const contractTxId = 't9T7DIOGxx4VWXoCEeYYarFYeERTpWIC1V3y-BPZgKE'; //749180
 
   //const interactionsLoader = new FromFileInteractionsLoader(path.join(__dirname, 'data', 'interactions.json'));
 
   // const smartweave = SmartWeaveWebFactory.memCachedBased(arweave).setInteractionsLoader(interactionsLoader).build();
   const smartweaveR = SmartWeaveWebFactory
-    .memCachedBased(arweave, 1).build();
+    .memCachedBased(arweave, 1)
+    .setInteractionsLoader(
+      new RedstoneGatewayInteractionsLoader("https://gateway.redstone.finance"))
+    .setDefinitionLoader(
+      new RedstoneGatewayContractDefinitionLoader("http://localhost:5666", arweave, new MemCache()))
+    .build();
 
  /* const usedBefore = Math.round((process.memoryUsage().heapUsed / 1024 / 1024) * 100) / 100
   const contractR = smartweaveR.contract(contractTxId);
@@ -51,11 +65,14 @@ async function main() {
   });
   const result = await contract.readState();
 
+  const result2 = await readContract(arweave, "t9T7DIOGxx4VWXoCEeYYarFYeERTpWIC1V3y-BPZgKE")
+
 
   //fs.writeFileSync(path.join(__dirname, 'data', 'validity.json'), JSON.stringify(validity));
 
   //fs.writeFileSync(path.join(__dirname, 'data', 'validity_old.json'), JSON.stringify(result.validity));
-  //fs.writeFileSync(path.join(__dirname, 'data', 'state_redstone.json'), JSON.stringify(state));
+  fs.writeFileSync(path.join(__dirname, 'data', 'state_new.json'), stringify(result.state).trim());
+  fs.writeFileSync(path.join(__dirname, 'data', 'state_old.json'), stringify(result2).trim());
   //fs.writeFileSync(path.join(__dirname, 'data', 'state_arweave.json'), JSON.stringify(result.state));
 
   // console.log('second read');
