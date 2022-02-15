f62dea1aae38c95d04ece71679326797443c9502
ppedziwiatr <peter@redstone.finance>
2022-02-15 11:56:00 +0100

    feat: sequencer should produce the sequence from the next block #47

--- src/__tests__/unit/squencer.test.ts
+++ src/__tests__/unit/squencer.test.ts
@@ -51,15 +51,15 @@ describe('Sequencer', () => {
     const sorted = await sorter.sort(transactions as unknown as GQLEdgeInterface[]);
 
     expect(sorted.map((s) => mapSorted(s))).toEqual([
-      '[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860512,1643210931796,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767',
-      '[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860512,1643210931797,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390',
-      '[cqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860512,1643210931798,558e456b249d174bc47ca665a3f6162c5ad61e63c12a1a868cc0e18d9e5cb768'
+      '[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860513,1643210931796,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767',
+      '[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860513,1643210931797,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390',
+      '[cqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860513,1643210931798,558e456b249d174bc47ca665a3f6162c5ad61e63c12a1a868cc0e18d9e5cb768'
     ]);
 
     // note that without the "millis" part, the ordering would be:
-    // "[cqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860512,558e456b249d174bc47ca665a3f6162c5ad61e63c12a1a868cc0e18d9e5cb768",
-    // "[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860512,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767",
-    // "[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860512,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390",
+    // "[cqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860513,558e456b249d174bc47ca665a3f6162c5ad61e63c12a1a868cc0e18d9e5cb768",
+    // "[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860513,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767",
+    // "[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860513,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390",
   });
 
   it('should properly assign sequence for transactions from different blocks', async () => {
@@ -119,13 +119,13 @@ describe('Sequencer', () => {
     const sorted = await sorter.sort(transactions as unknown as GQLEdgeInterface[]);
 
     expect(sorted.map((s) => mapSorted(s))).toEqual([
-      '[ATzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860511,1643210931796,0233bf40009ae4ae32d6125cf5375da8d77cb024a86b26623cf8e9816b4b7a48',
-      '[0dFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860511,1643210931797,580a0405284ac42c75c2336c0efac2c5fcd3b379b4f51b7f842a501a704c1038',
-      '[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860512,1643210931798,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767',
-      '[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860512,1643210931799,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390',
-      '[cqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860512,1643210931800,558e456b249d174bc47ca665a3f6162c5ad61e63c12a1a868cc0e18d9e5cb768',
-      '[CdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860513,1643210931801,26844bd597465eae300253dd923bad9db037e15f4f6bd26e1f4a9aac27152f4f',
-      '[DqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860513,1643210931802,854d60e74c8c9298dfa72ce9fda56c5e7b40aca41dd47290c5ff17fb3a94e87d'
+      '[ATzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860512,1643210931796,0233bf40009ae4ae32d6125cf5375da8d77cb024a86b26623cf8e9816b4b7a48',
+      '[0dFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860512,1643210931797,580a0405284ac42c75c2336c0efac2c5fcd3b379b4f51b7f842a501a704c1038',
+      '[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860513,1643210931798,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767',
+      '[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860513,1643210931799,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390',
+      '[cqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860513,1643210931800,558e456b249d174bc47ca665a3f6162c5ad61e63c12a1a868cc0e18d9e5cb768',
+      '[CdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860514,1643210931801,26844bd597465eae300253dd923bad9db037e15f4f6bd26e1f4a9aac27152f4f',
+      '[DqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860514,1643210931802,854d60e74c8c9298dfa72ce9fda56c5e7b40aca41dd47290c5ff17fb3a94e87d'
     ]);
   });
 
@@ -186,13 +186,13 @@ describe('Sequencer', () => {
     const sorted = await sorter.sort(transactions as unknown as GQLEdgeInterface[]);
 
     expect(sorted.map((s) => mapSorted(s))).toEqual([
-      '[BdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860511,1643210931797,ce88782c6a2005ef504ef74223fc864e8cef2aa158cea3dec2266b6871e9aae0',
       '[ATzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860511,9999999999999,e2199366ba729c3cb9580b0757858689a66e3435aa7462ffa466eba2b2f8f449',
-      '[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860512,1643210931798,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767',
-      '[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860512,1643210931799,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390',
+      '[BdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860512,1643210931797,ce88782c6a2005ef504ef74223fc864e8cef2aa158cea3dec2266b6871e9aae0',
       '[cqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860512,9999999999999,bb6d3084dd5770a43234e253154fb75cd886b57adf3df52901bf67789247b9b9',
-      '[DqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860513,1643210931802,854d60e74c8c9298dfa72ce9fda56c5e7b40aca41dd47290c5ff17fb3a94e87d',
-      '[CdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860513,9999999999999,12d814fd50a9fabcf334075b3dd5d4e8d4455fb729b6063b092967c99143e084'
+      '[JTzWaG_tiagnoCNlPpdWvN1r16rw47MbNgZn6Fd1dNk] : 000000860513,1643210931798,81e1bea09d3262ee36ce8cfdbbb2ce3feb18a717c3020c47d206cb8ecb43b767',
+      '[UdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860513,1643210931799,ba45da3dc691d607f94aa90e5e2121928164b624872f277d708ed807330eb390',
+      '[CdFlfj5IUYh-7lHJuLLC94P9M1mqxKhkpuuiMzKUKss] : 000000860513,9999999999999,12d814fd50a9fabcf334075b3dd5d4e8d4455fb729b6063b092967c99143e084',
+      '[DqehSNlOvnqHoRRs56Cex8PEhHsaGYLz1m4j19kWB-0] : 000000860514,1643210931802,854d60e74c8c9298dfa72ce9fda56c5e7b40aca41dd47290c5ff17fb3a94e87d'
     ]);
   });
 
@@ -244,7 +244,7 @@ async function createSortKey(
   const jwkDBytes = arweave.utils.b64UrlToBuffer(jwk.d as string);
   const concatenated = arweave.utils.concatBuffers([blockHashBytes, txIdBytes, jwkDBytes]);
   const hashed = arrayToHex(await arweave.crypto.hash(concatenated));
-  const blockHeightString = `${blockHeight}`.padStart(12, '0');
+  const blockHeightString = `${blockHeight + 1}`.padStart(12, '0');
 
   return `${blockHeightString},${mills},${hashed}`;
 }
