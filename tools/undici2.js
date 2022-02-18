caac6dd6e160356b46d980701d9d2818228d5eaa
ppedziwiatr <peter@redstone.finance>
2022-02-18 11:30:18 +0100

    bug: random "Unable to retrieve tx" erros while loading contract's data #97

--- tools/undici2.js
+++ tools/undici2.js
@@ -1,13 +1,11 @@
 /* eslint-disable */
 
-const Arweave = require("arweave");
-
 const Undici = require("undici");
 const fetch = Undici.fetch;
 
 async function test() {
   for (let i = 0; i < 100; i++) {
-    const response = await fetch(`https://arweave.net:443/tx/4o-2xMPa45BXjGuII_LbOMQWfhE1F0qugdEUZvRlXRY`)
+    const response = await fetch(`https://arweave.net:443/tx/ydjfv0hRQdD2c-MASv4L5Qahjap_LXJyD9tNyKWvf50`)
       .then((res) => {
         return res.ok ? res.json() : Promise.reject(res);
       })
@@ -19,12 +17,6 @@ async function test() {
       });
 
     console.log(response);
-
-
-    /*const response = await fetch('https://arweave.net:443/4o-2xMPa45BXjGuII_LbOMQWfhE1F0qugdEUZvRlXRY');
-    const buffer = await response.arrayBuffer();
-    const result = Arweave.utils.bufferToString(buffer);
-    console.log(result);*/
   }
 }
 
