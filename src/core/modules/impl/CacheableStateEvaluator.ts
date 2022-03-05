d9bef299defc6fa5c61746622b0481cf12b9f5ca
ppedziwiatr <peter@redstone.finance>
2022-03-05 17:01:33 +0100

    fix: init state from wasm contracts when state fully cached

--- src/core/modules/impl/CacheableStateEvaluator.ts
+++ src/core/modules/impl/CacheableStateEvaluator.ts
@@ -40,6 +40,7 @@ export class CacheableStateEvaluator extends DefaultStateEvaluator {
 
     const cachedState = executionContext.cachedState;
     if (cachedState?.cachedHeight === requestedBlockHeight) {
+      executionContext.handler?.initState(cachedState.cachedValue.state);
       return cachedState.cachedValue;
     }
 
@@ -93,6 +94,7 @@ export class CacheableStateEvaluator extends DefaultStateEvaluator {
       // if cache is up-to date - return immediately to speed-up the whole process
       if (missingInteractions.length === 0 && cachedState) {
         this.cLogger.debug(`State up to requested height [${requestedBlockHeight}] fully cached!`);
+        executionContext.handler?.initState(cachedState.cachedValue.state);
         return cachedState.cachedValue;
       }
     }
