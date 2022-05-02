378e734dde640070bd0973e8dd42e634eef4e2fa
ppe <peter@redstone.finance>
2022-05-02 20:10:46 +0200

    feat: add flag to disallow state evaluation when using unsafeClient #136

---
+++ src/__tests__/integration/basic/unsafe-contract.test.ts
@@ -0,0 +1,72 @@
+import fs from 'fs';
+
+import ArLocal from 'arlocal';
+import Arweave from 'arweave';
+import { JWKInterface } from 'arweave/node/lib/wallet';
+import { Contract, LoggerFactory, SmartWeave, SmartWeaveNodeFactory } from '@smartweave';
+import path from 'path';
+import { addFunds, mineBlock } from '../_helpers';
+
+let arweave: Arweave;
+let arlocal: ArLocal;
+let smartweave: SmartWeave;
+let contract: Contract<any>;
+let contractWithUnsafe: Contract<any>;
+
+describe('Testing the SmartWeave client', () => {
+  let contractSrc: string;
+
+  let wallet: JWKInterface;
+
+  beforeAll(async () => {
+    // note: each tests suit (i.e. file with tests that Jest is running concurrently
+    // with another files has to have ArLocal set to a different port!)
+    arlocal = new ArLocal(1801, false);
+    await arlocal.start();
+
+    arweave = Arweave.init({
+      host: 'localhost',
+      port: 1801,
+      protocol: 'http'
+    });
+
+    LoggerFactory.INST.logLevel('error');
+
+    smartweave = SmartWeaveNodeFactory.memCached(arweave);
+
+    wallet = await arweave.wallets.generate();
+    await addFunds(arweave, wallet);
+
+    contractSrc = fs.readFileSync(path.join(__dirname, '../data/token-pst-unsafe.js'), 'utf8');
+
+    // deploying contract using the new SDK.
+    const contractTxId = await smartweave.createContract.deploy({
+      wallet,
+      initState: JSON.stringify({}),
+      src: contractSrc
+    });
+
+    contract = smartweave.contract(contractTxId);
+    contractWithUnsafe = smartweave.contract(contractTxId).setEvaluationOptions({
+      allowUnsafeClient: true
+    });
+    contract.connect(wallet);
+    contractWithUnsafe.connect(wallet);
+
+    await mineBlock(arweave);
+  });
+
+  afterAll(async () => {
+    await arlocal.stop();
+  });
+
+  it('should not allow to evaluate contract with unsafe operations by default', async () => {
+    await expect(contract.readState()).rejects.toThrowError(
+      'Using unsafeClient is not allowed by default. Use EvaluationOptions.allowUnsafeClient flag.'
+    );
+  });
+
+  it('should allow to evaluate contract with unsafe operations by when evaluation option is set.', async () => {
+    expect(await contractWithUnsafe.readState()).not.toBeUndefined();
+  });
+});
