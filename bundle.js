f6f54e7723f2c838a9a0d6d45405581db088c651
ppe <peter@redstone.finance>
2022-08-24 15:23:46 +0200

    feat: esm wrapper for cjs, instead of separate build

--- bundle.js
+++ bundle.js
@@ -10,25 +10,6 @@ const clean = async () => {
 const runBuild = async () => {
   await clean();
 
-  const iifeBuild = {
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    platform: 'browser',
-    target: ['esnext'],
-    format: 'iife',
-    globalName: 'warp'
-  };
-
-  console.log('Building web legacy bundle.');
-  build({
-    ...iifeBuild,
-    minify: true,
-    outfile: './bundles/web.iife.bundle.min.js'
-  }).catch((e) => {
-    console.log(e);
-    process.exit(1);
-  });
-
   const esmBuild = {
     entryPoints: ['./src/index.ts'],
     bundle: true,
@@ -41,41 +22,12 @@ const runBuild = async () => {
   console.log('Building web bundle.');
   build({
     ...esmBuild,
-    minify: false,
+    minify: true,
     outfile: './bundles/web.bundle.min.js'
   }).catch((e) => {
     console.log(e);
     process.exit(1);
   });
-
-  console.log('Building node bundle.');
-  build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    platform: 'node',
-    target: ['esnext'],
-    format: 'cjs',
-    minify: true,
-    outfile: './bundles/node.bundle.min.js'
-  }).catch((e) => {
-    console.log(e);
-    process.exit(1);
-  });
-
-  console.log('Building web bundle.');
-  build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    platform: 'browser',
-    target: ['esnext'],
-    format: 'cjs',
-    minify: true,
-    outfile: './bundles/web.next.bundle.min.js'
-  }).catch((e) => {
-    console.log(e);
-    process.exit(1);
-  });
-
 };
 runBuild().finally(() => {
   console.log('Build done.');
