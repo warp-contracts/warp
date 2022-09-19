d2541bca598e41c75aca6b5642ca164f5736d807
ppe <peter@redstone.finance>
2022-09-19 12:16:21 +0200

    v1.2.6

--- src/__tests__/integration/internal-writes/internal-write-aftr.test.ts
+++ src/__tests__/integration/internal-writes/internal-write-aftr.test.ts
@@ -2,13 +2,13 @@
 import fs from 'fs';
 
 import ArLocal from 'arlocal';
-import {JWKInterface} from 'arweave/node/lib/wallet';
+import { JWKInterface } from 'arweave/node/lib/wallet';
 import path from 'path';
-import {mineBlock} from '../_helpers';
-import {Contract} from '../../../contract/Contract';
-import {Warp} from '../../../core/Warp';
-import {WarpFactory} from '../../../core/WarpFactory';
-import {LoggerFactory} from '../../../logging/LoggerFactory';
+import { mineBlock } from '../_helpers';
+import { Contract } from '../../../contract/Contract';
+import { Warp } from '../../../core/Warp';
+import { WarpFactory } from '../../../core/WarpFactory';
+import { LoggerFactory } from '../../../logging/LoggerFactory';
 
 interface ExampleContractState {
   counter: number;
@@ -34,7 +34,6 @@ describe('Testing internal writes', () => {
 
   let originalTxId; // set during tests, yeah it sucks, but fuck it for now.
 
-
   const port = 1966;
 
   beforeAll(async () => {
@@ -52,21 +51,24 @@ describe('Testing internal writes', () => {
 
   async function deployContracts() {
     warp = WarpFactory.forLocal(port);
-    ({jwk: wallet, address: walletAddress} = await warp.testing.generateWallet());
+    ({ jwk: wallet, address: walletAddress } = await warp.testing.generateWallet());
 
     aftrContractSrc = fs.readFileSync(path.join(__dirname, '../data/aftr/sampleContractSrc.js'), 'utf8');
-    aftrContractInitialState = fs.readFileSync(path.join(__dirname, '../data/aftr/sampleContractInitState.json'), 'utf8');
+    aftrContractInitialState = fs.readFileSync(
+      path.join(__dirname, '../data/aftr/sampleContractInitState.json'),
+      'utf8'
+    );
 
     pstContractSrc = fs.readFileSync(path.join(__dirname, '../data/aftr/sampleContractSrc.js'), 'utf8');
     pstInitialState = fs.readFileSync(path.join(__dirname, '../data/aftr/pstInitState.json'), 'utf8');
 
-    ({contractTxId: aftrTxId, srcTxId: aftrSrcTxId} = await warp.createContract.deploy({
+    ({ contractTxId: aftrTxId, srcTxId: aftrSrcTxId } = await warp.createContract.deploy({
       wallet,
       initState: aftrContractInitialState,
       src: aftrContractSrc
     }));
 
-    ({contractTxId: pstTxId} = await warp.createContract.deployFromSourceTx({
+    ({ contractTxId: pstTxId } = await warp.createContract.deployFromSourceTx({
       wallet,
       initState: pstInitialState,
       srcTxId: aftrSrcTxId
@@ -102,7 +104,7 @@ describe('Testing internal writes', () => {
 
       // (o) mint 10000 tokens in pst contract
       await pst.writeInteraction({
-        function: "mint",
+        function: 'mint',
         qty: 10000
       });
 
@@ -110,77 +112,53 @@ describe('Testing internal writes', () => {
 
       // (o) verify pst state after minting the tokens
       expect(pstState.cachedValue.state).toEqual({
-        "name": "Vint",
-        "ticker": "VINT",
-        "balances": {
+        name: 'Vint',
+        ticker: 'VINT',
+        balances: {
           [walletAddress]: 10000
         },
-        "claimable": [],
-        "claims": [],
-        "settings": [
-          [
-            "quorum",
-            0.5
-          ],
-          [
-            "support",
-            0.5
-          ],
-          [
-            "voteLength",
-            2160
-          ],
-          [
-            "lockMinLength",
-            129600
-          ],
-          [
-            "lockMaxLength",
-            1051200
-          ],
-          [
-            "communityAppUrl",
-            ""
-          ],
-          [
-            "communityDiscussionLinks",
-            [
-              ""
-            ]
-          ],
-          [
-            "communityDescription",
-            ""
-          ],
-          [
-            "communityLogo",
-            ""
-          ]
+        claimable: [],
+        claims: [],
+        settings: [
+          ['quorum', 0.5],
+          ['support', 0.5],
+          ['voteLength', 2160],
+          ['lockMinLength', 129600],
+          ['lockMaxLength', 1051200],
+          ['communityAppUrl', ''],
+          ['communityDiscussionLinks', ['']],
+          ['communityDescription', ''],
+          ['communityLogo', '']
         ]
-      })
+      });
     });
 
     // this is the equivalent of the original "buttonPress3"
     it('[buttonPress3] should set allowance and make a deposit', async () => {
-
       const transferQty = 1;
       // (o) set allowance on pst contract for aftr contract
-      ({originalTxId} = await pst.writeInteraction({
-        function: "allow",
-        target: aftrTxId,
-        qty: transferQty,
-      }, {strict: true}));
+      ({ originalTxId } = await pst.writeInteraction(
+        {
+          function: 'allow',
+          target: aftrTxId,
+          qty: transferQty
+        },
+        { strict: true }
+      ));
 
       // (o) make a deposit transaction on the aftr contract
       // note: this transaction makes internalWrite on pst contract
       // i.e. it calls PST's 'claim' function
-      console.log("============= DEPOSIT TX =========")
-      await aftr.writeInteraction({
-        function: "deposit",
-        tokenId: pstTxId,
-        qty: transferQty,
-        txID: originalTxId,
-      }, {strict: true});
+      console.log('============= DEPOSIT TX =========');
+      await aftr.writeInteraction(
+        {
+          function: 'deposit',
+          tokenId: pstTxId,
+          qty: transferQty,
+          txID: originalTxId
+        },
+        { strict: true }
+      );
 
       const aftrState = await aftr.readState();
       expect(aftrState.cachedValue.state).toEqual({
@@ -240,24 +218,21 @@ describe('Testing internal writes', () => {
           ['lockMinLength', 129600],
           ['lockMaxLength', 1051200],
           ['communityAppUrl', ''],
-          ['communityDiscussionLinks', [""]],
+          ['communityDiscussionLinks', ['']],
           ['communityDescription', ''],
           ['communityLogo', '']
         ]
       });
     });
-
   });
 
   describe('AFTR test case - with a fresh Warp instance', () => {
     it('should properly read PST contract state', async () => {
       const newWarpInstance = WarpFactory.forLocal(port);
 
-      const pst2 = newWarpInstance
-        .contract<ExampleContractState>(pstTxId)
-        .setEvaluationOptions({
-          internalWrites: true,
-        });
+      const pst2 = newWarpInstance.contract<ExampleContractState>(pstTxId).setEvaluationOptions({
+        internalWrites: true
+      });
 
       const pstState = await pst2.readState();
       expect(pstState.cachedValue.state).toEqual({
@@ -276,7 +251,7 @@ describe('Testing internal writes', () => {
           ['lockMinLength', 129600],
           ['lockMaxLength', 1051200],
           ['communityAppUrl', ''],
-          ['communityDiscussionLinks', [""]],
+          ['communityDiscussionLinks', ['']],
           ['communityDescription', ''],
           ['communityLogo', '']
         ]
@@ -284,24 +259,29 @@ describe('Testing internal writes', () => {
     });
   });
 
-
   describe('AFTR test case - with an illegal read state after an internal write', () => {
     it('should throw an Error if contract makes readContractState after write on the same contract', async () => {
       const newWarpInstance = WarpFactory.forLocal(port);
       const { jwk: wallet2 } = await newWarpInstance.testing.generateWallet();
 
-      const aftrBrokenContractSrc = fs.readFileSync(path.join(__dirname, '../data/aftr/sampleContractSrc_broken.js'), 'utf8');
+      const aftrBrokenContractSrc = fs.readFileSync(
+        path.join(__dirname, '../data/aftr/sampleContractSrc_broken.js'),
+        'utf8'
+      );
 
-      const aftrBrokenContractInitialState = fs.readFileSync(path.join(__dirname, '../data/aftr/sampleContractInitState.json'), 'utf8');
+      const aftrBrokenContractInitialState = fs.readFileSync(
+        path.join(__dirname, '../data/aftr/sampleContractInitState.json'),
+        'utf8'
+      );
       const pst2InitState = fs.readFileSync(path.join(__dirname, '../data/aftr/pstInitState.json'), 'utf8');
 
-      const {contractTxId: aftrBrokenTxId, srcTxId: brokenSrcTxId} = await newWarpInstance.createContract.deploy({
+      const { contractTxId: aftrBrokenTxId, srcTxId: brokenSrcTxId } = await newWarpInstance.createContract.deploy({
         wallet: wallet2,
         initState: aftrBrokenContractInitialState,
         src: aftrBrokenContractSrc
       });
 
-      const {contractTxId: pst2TxId} = await newWarpInstance.createContract.deployFromSourceTx({
+      const { contractTxId: pst2TxId } = await newWarpInstance.createContract.deployFromSourceTx({
         wallet: wallet2,
         initState: pst2InitState,
         srcTxId: brokenSrcTxId
@@ -325,28 +305,33 @@ describe('Testing internal writes', () => {
 
       // (o) mint 10000 tokens in pst contract
       await pst2.writeInteraction({
-        function: "mint",
+        function: 'mint',
         qty: 10000
       });
 
       const transferQty = 1;
       // (o) set allowance on pst contract for aftr contract
-      const {originalTxId} = await pst2.writeInteraction({
-        function: "allow",
+      const { originalTxId } = await pst2.writeInteraction({
+        function: 'allow',
         target: aftrBrokenTxId,
-        qty: transferQty,
+        qty: transferQty
       });
 
       // (o) make a deposit transaction on the AFTR contract
       // note: this transaction makes internalWrite on PST contract
       // and then makes a readContractState on a PST contract
       // - such operation is not allowed.
-      await expect(aftrBroken.writeInteraction({
-        function: "deposit",
-        tokenId: pst2TxId,
-        qty: transferQty,
-        txID: originalTxId,
-      }, { strict: true })).rejects.toThrowError(
+      await expect(
+        aftrBroken.writeInteraction(
+          {
+            function: 'deposit',
+            tokenId: pst2TxId,
+            qty: transferQty,
+            txID: originalTxId
+          },
+          { strict: true }
+        )
+      ).rejects.toThrowError(
         /Calling a readContractState after performing an inner write is wrong - instead use a state from the result of an internal write./
       );
     });
