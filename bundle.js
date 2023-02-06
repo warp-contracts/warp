d6d432ebc9a72d941d67b520a5629ecb7713bbdb
ppe <peter@redstone.finance>
2023-02-06 14:50:27 +0100

    chore: do not generate metadata for esbuild

--- bundle.js
+++ bundle.js
@@ -24,17 +24,17 @@ const runBuild = async () => {
   const result = await build({
     ...buildConfig,
     minify: true,
-    metafile: true,
+    // metafile: true,
     outfile: './bundles/web.bundle.min.js'
   }).catch((e) => {
     console.log(e);
     process.exit(1);
   });
 
-  fs.writeFileSync('metadata.json', JSON.stringify({
+  /*fs.writeFileSync('metadata.json', JSON.stringify({
     inputs: result.metafile.inputs,
     outputs: result.metafile.outputs
-  }));
+  }));*/
 
   console.log('Building web bundle iife.');
   await build({
