ecdb6f9dd953d121ee2d7ca4045718c1cbae82d8
Asia <ziola.jm@gmail.com>
2024-08-12 09:16:40 +0200

    v1.4.36-beta.4

--- src/core/modules/impl/DefaultStateEvaluator.ts
+++ src/core/modules/impl/DefaultStateEvaluator.ts
@@ -344,9 +344,13 @@ export abstract class DefaultStateEvaluator implements StateEvaluator {
           this.logger.info('Update benchmark', updateBenchmark.elapsed());
           if (validity[missingInteraction.id]) {
             const commitBenchmark = Benchmark.measure();
-            await contract.interactionState().commit(missingInteraction, forceStateStoreToCache);
-            commitBenchmark.stop();
-            this.logger.info('Commit benchmark', commitBenchmark.elapsed());
+            contract
+              .interactionState()
+              .commit(missingInteraction, forceStateStoreToCache)
+              .then(() => {
+                commitBenchmark.stop();
+                this.logger.info('Commit benchmark', commitBenchmark.elapsed());
+              });
           } else {
             const rollbackBenchmark = Benchmark.measure();
             await contract.interactionState().rollback(missingInteraction, forceStateStoreToCache);
